12c97252929b84eabba55dcfacdbda1a78e4faf4
Junkwolves <justajunk2069@gmail.com>
2018-12-25 17:15:28 +0200

    For_Each
    C++ For Each Information.

---
+++ For_Each/for_each/for_each/for_each.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+int El_Count = 0;
+
+int main()
+{
+	int arr[] = {2, 6, 8, 3};
+
+	std::cout << std::endl;
+
+	/* 
+		// Old version
+
+		for each(int var int arr)
+		{
+			std::cout << var << std::endl;
+		}
+
+	*/
+
+	// A new version
+
+	for (int it : arr)
+	{
+		std::cout << std::endl << "   |      arr[" << El_Count << "] = "<< it << std::endl;
+		El_Count++;
+	}
+
+	El_Count = 0;
+
+	std::cout << std::endl << " --------------------------------------------------------- " << std::endl;
+
+	for (int &it : arr)
+	{
+
+		it = 1; // arr[it] = 1;
+
+		std::cout << std::endl << "   |      arr[" << El_Count << "] = " << it << std::endl;
+		El_Count++;
+	}
+
+	El_Count = 0;
+
+	std::cout << std::endl << " --------------------------------------------------------- " << std::endl;
+
+	std::vector<int> myVector = {4, 5, 6};
+
+	for (int it : myVector)
+	{
+		std::cout << std::endl << "   |      myVector[" << El_Count << "] = " << it << std::endl;
+		El_Count++;
+	}
+
+	El_Count = 0;
+
+	char a[100]; std::cin >> a;
+}
