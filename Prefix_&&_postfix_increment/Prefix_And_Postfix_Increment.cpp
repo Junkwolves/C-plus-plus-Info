fe7b6d82a6cb23c6385e52204216391257479819
Junkwolves <justajunk2069@gmail.com>
2018-12-27 11:19:33 +0200

    Prefix_And_Postfix_Increment
    Prefix and postfix increment example

---
+++ Prefix_&&_postfix_increment/Prefix_And_Postfix_Increment.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a1 = 0;
+
+	cout << endl << "     | ---------------------- |  " << endl;
+
+	cout << endl << "     |  a1 = 0;  |  a1++ = " << a1++ << "  |  " << endl;
+
+	int a2 = 0;
+
+	cout << endl << "     |  a2 = 0;  |  ++a2 = " << ++a2 << "  |  " << endl;
+
+	cout << endl << "     | ---------------------- |  " << endl;
+
+	cout << endl << "     |  a1 = 1;  |  a1-- = " << a1-- << "  |  " << endl;
+
+	cout << endl << "     |  a2 = 1;  |  --a2 = " << --a2 << "  |  " << endl;
+
+	cout << endl << "     | ---------------------- |  " << endl;
+
+	return 0;
+}
